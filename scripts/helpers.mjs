8b80837162498a005597e6560b2ba4dd196c2b99
TeyeJeff <teyejeff18@gmail.com>
2026-02-10 04:07:59 +0000

    skeleton 100% complete

--- scripts/helpers.mjs
+++ scripts/helpers.mjs
@@ -86,3 +86,13 @@ function addHeaderEventListeners() {
         });
     }
 }
+
+// function to retrieve data to local storage
+export function getLocalStorage(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
+
+// function to save data inot local storage 
+export function setLocalStorage(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
